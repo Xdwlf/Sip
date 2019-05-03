4ec870a6f3f3b74dcfb1518db0a92607edf1233f
Rowan Lin <xdwlf32@gmail.com>
2019-05-03 16:58:43 -0700

    Create AuthForm for signin and signup purposes

---
+++ src/components/AuthForm.js
@@ -0,0 +1,94 @@
+import React, {Component} from 'react'
+
+class AuthForm extends Component{
+    constructor(props){
+        super(props)
+        this.state= {
+            email: "",
+            username: "",
+            password: "",
+            profileImgUrl: ""
+        }
+    }
+
+    handleChange= (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    handleSubmit = (e)=>{
+        e.preventDefault();
+    }
+
+    render(){
+        const {signUp, header, btnText} = this.props
+        return (
+            <section>
+                <form id="auth-form" onSubmit={this.handleSubmit}>
+                    <h4>{header}</h4>
+                    <div className="form-group row">
+                        <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
+                        <div className="col-sm-10">
+                            <input type="text" 
+                                className="form-control"
+                                id="email" 
+                                name="email" 
+                                value={this.state.email}
+                                placeholder="Your Email"
+                                onChange={this.handleChange}
+                                />
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <label htmlFor="password" className="col-sm-2 col-form-label">Password</label>
+                        <div className="col-sm-10">
+                            <input type="password" 
+                                className="form-control"
+                                id="password" 
+                                name="password" 
+                                placeholder="Password"
+                                value={this.state.password}
+                                onChange={this.handleChange}                                
+                                />
+                        </div>
+                    </div>
+
+                    {signUp && 
+                    <div>
+                        <div className="form-group row">
+                            <label htmlFor="username" className="col-sm-2 col-form-label">Username</label>
+                            <div className="col-sm-10">
+                                <input type="text" 
+                                    id="username" 
+                                    className="form-control"
+                                    name="username" 
+                                    placeholder="Username"
+                                    value={this.state.username}
+                                    onChange={this.handleChange}
+                                    />
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label htmlFor="profileImgUrl" className="col-sm-2 col-form-label">Image Url</label>
+                            <div className="col-sm-10">
+                                <input type="text" 
+                                    id="profileImgUrl" 
+                                    className="form-control"
+                                    name="profileImgUrl" 
+                                    placeholder="Profile Image Url"
+                                    value={this.state.profileImgUrl}
+                                    onChange={this.handleChange}                                    
+                                    />
+                            </div>
+                        </div>
+                    </div>
+                    }
+                    <button className="btn btn-outline-success btn-block" type="submit">{btnText}</button>
+                </form>
+            </section>
+        )
+    }
+}
+
+export default AuthForm; 
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,6 +3,7 @@ import {Switch, Route} from "react-router-dom"
 import SearchPage from './SearchPage'
 import Homepage from "./Homepage"
 import SideBar from './SideBar'
+import AuthForm from "../components/AuthForm"
 import NotFound from "../components/NotFound"
 import DrinkItem from "../components/DrinkItem"
 
@@ -17,6 +18,19 @@ class Main extends Component{
                     <Switch>
                         <Route exact path="/" component={Homepage}/>
                         <Route exact path="/search" component={SearchPage}/>
+                        <Route exac path="/signup" render={props=> {
+                            return <AuthForm signUp={true} 
+                                        btnText="Sign Up Today!"
+                                        header="Create a New Account"
+                            />
+                        }} />
+                        <Route exac path="/signin" render={props=> {
+                            return <AuthForm signUp={false} 
+                                        btnText="Submit"
+                                        header="Log In"
+                            
+                            />
+                        }} />
                         <Route path="/drinks/:drinkId" component={DrinkItem} />
                         <Route component={NotFound} />
                     </Switch>
