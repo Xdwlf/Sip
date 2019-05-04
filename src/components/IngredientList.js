ad534bd7ac206e0e937737007380b8c6be268b73
Rowan Lin <xdwlf32@gmail.com>
2019-05-03 19:37:02 -0700

    make more robust with more checks to prevent errors

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -4,7 +4,10 @@ const IngredientList = ({drink}) => {
     let ingredients= [];
     let instructions = []
     for(let i = 1; i<16; i++){
-        if(drink["strIngredient"+i].length>0){
+        if(drink["strIngredient"+i]&&
+            (drink["strMeasure"+1])&& 
+            drink["strIngredient"+i].length>0
+            ){
             ingredients.push(drink["strIngredient"+i]);
             instructions.push(drink["strMeasure"+1]);
         }
